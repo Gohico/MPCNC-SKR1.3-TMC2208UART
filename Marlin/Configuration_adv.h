48df8129129a43ec6967818f9ca976ca06a6aaf6
nbecke <nbecke@posteo.de>
2020-04-18 21:49:13 +0200

    Back to 800

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1853,7 +1853,7 @@
 //0.8A RMS --> 1.13VRef
 
 #if AXIS_IS_TMC(X)
-#define X_CURRENT 700            // (mA) RMS current. Multiply by 1.414 for peak current.
+#define X_CURRENT 800            // (mA) RMS current. Multiply by 1.414 for peak current.
 #define X_CURRENT_HOME X_CURRENT // (mA) RMS current for sensorless homing
 #define X_MICROSTEPS 16          // 0..256
 #define X_RSENSE 0.11
@@ -1861,7 +1861,7 @@
 #endif
 
 #if AXIS_IS_TMC(X2)
-#define X2_CURRENT 700
+#define X2_CURRENT 800
 #define X2_CURRENT_HOME X2_CURRENT
 #define X2_MICROSTEPS 16
 #define X2_RSENSE 0.11
@@ -1869,7 +1869,7 @@
 #endif
 
 #if AXIS_IS_TMC(Y)
-#define Y_CURRENT 700
+#define Y_CURRENT 800
 #define Y_CURRENT_HOME Y_CURRENT
 #define Y_MICROSTEPS 16
 #define Y_RSENSE 0.11
@@ -1877,7 +1877,7 @@
 #endif
 
 #if AXIS_IS_TMC(Y2)
-#define Y2_CURRENT 700
+#define Y2_CURRENT 800
 #define Y2_CURRENT_HOME Y2_CURRENT
 #define Y2_MICROSTEPS 16
 #define Y2_RSENSE 0.11
@@ -1885,7 +1885,7 @@
 #endif
 
 #if AXIS_IS_TMC(Z)
-#define Z_CURRENT 700
+#define Z_CURRENT 800
 #define Z_CURRENT_HOME Z_CURRENT
 #define Z_MICROSTEPS 16
 #define Z_RSENSE 0.11
@@ -1909,7 +1909,7 @@
 #endif
 
 #if AXIS_IS_TMC(E0)
-#define E0_CURRENT 700
+#define E0_CURRENT 800
 #define E0_MICROSTEPS 16
 #define E0_RSENSE 0.11
 #define E0_CHAIN_POS -1
